9a1c6253d28644366e19cf65d2e9c62d747d5b44
viczaq <C108118149@nkust.edu.tw>
2020-09-21 16:33:24 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>何敏煌0903</title>
+</head>
+<body>
+<h2>何敏煌0903 PHP練習記錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
